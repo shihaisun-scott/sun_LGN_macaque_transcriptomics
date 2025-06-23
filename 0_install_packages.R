4b638d270e3708f89a944bb9935de7255370fd89
shihaisun-scott <shihaisun.scott@gmail.com>
2025-06-23 10:40:29 -0400

    Update 0_install_packages.R

--- 0_install_packages.R
+++ 0_install_packages.R
@@ -3,4 +3,16 @@ install.packages("BiocManager")
 BiocManager::install("edgeR")     # Install edgeR using BiocManager
 install.packages("gplots")  # Install gplots if not already installed
 BiocManager::install("SeuratWrappers")
+install.packages("dplyr")
+install.packages("cowplot")
+install.packages("gridExtra")
+install.packages("cluster")
+install.packages("mgcv")
+install.packages("Matrix")
+install.packages("clue")
+install.packages("igraph")
+install.packages("tidyr")
+install.packages("openxlsx")
+install.packages("ggrepel")
+install.packages("patchwork")
 
